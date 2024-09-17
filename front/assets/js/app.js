be278b3d24be63aece37fee6fcca36295693b3f4
camille <camille.nerriere@yahoo.fr>
2024-09-17 13:28:34 +0200

    mise en place stucture index et début modales

---
+++ front/assets/js/app.js
@@ -0,0 +1,5 @@
+
+// affichage de la modale pour les détails du pokémons 
+document.getElementById('bulbizarreName').addEventListener('click', function() {
+    $('#pokemonDetailModal').modal('show');
+});
