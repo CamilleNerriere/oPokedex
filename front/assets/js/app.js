5c109e20acabfb2fc50409a2b85659f92bd48365
camille <camille.nerriere@yahoo.fr>
2024-09-18 10:42:12 +0200

    fin html et css de base

--- front/assets/js/app.js
+++ front/assets/js/app.js
@@ -1,10 +1,20 @@
-
-// affichage de la modale pour les détails du pokémons 
-document.getElementById('bulbizarreName').addEventListener('click', function() {
-    $('#pokemonDetailModal').modal('show');
+document.querySelector('#bulbizarreName').addEventListener('click', function() {
+    // Affiche la modale Pokémon
+    const pokemonModal = new bootstrap.Modal(document.getElementById('pokemonDetailModal'));
+    pokemonModal.show();
 });
 
+// Affichage de la modale pour les détails de l'équipe
 document.querySelector('.itemDetailsLink').addEventListener('click', function() {
-    $('#teamDetailModal').modal('show');
+    // Affiche la modale d'équipe
+    const teamModal = new bootstrap.Modal(document.getElementById('teamDetailModal'));
+    teamModal.show();
 });
 
+// Affichage ajouter une équipe : 
+
+document.querySelector('.addTeamBtn').addEventListener('click', function() {
+    // Affiche la modale ajout équipe
+    const addTeamModal = new bootstrap.Modal(document.getElementById('addTeamModal'));
+    addTeamModal.show();
+});
